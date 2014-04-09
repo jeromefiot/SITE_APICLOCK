4f97520689b68fb542e4913c49eb3701d75ee626
jeromefiot <j_fiot@hotmail.com>
2014-04-09 15:16:53 +0200

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from app import app
+from flask import render_template, flash, redirect, session, url_for
+from forms import LoginForm
+
+
+@app.route('/')
+def index():
+    title="Accueil"
+    return render_template("index.html",
+                           title=title)
+
+@app.route('/presentation')
+def presentation():
+    title="Presentation Apiclock"
+
+    return render_template("apiclock_presentation.html",
+                           title=title)
+
+
+@app.route('/login',methods=['POST', 'GET'])
+def login():
+    form=LoginForm()
+    title="Login"
+
+    # Si retour de validation de formulaire
+    if form.validate_on_submit():
+        flash('Vous etes connectes '+ form.pseudo.data +' avec l\'id' + form.openid.data +', have a nice day' )
+        return redirect(url_for('index'))
+
+    return render_template("login.html",
+                           title=title  ,
+                           form=form,
+                           providers = app.config['OPENID_PROVIDERS'])
---
+++ forms.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from flask.ext.wtf import Form
+from wtforms import TextField, BooleanField, TextAreaField
+from wtforms.validators import Required, Length
+
+
+class LoginForm(Form):
+    """ ... """
+    email=TextField('email', validators=[Length(min=8, max=40)])
+    pseudo=TextField('username', validators=[Length(min=4, max=40)])
+    agree=BooleanField('On est gentils mais acceptez les condition d\'utilisation', validators = [Required()])
+    openid = TextField('openid', validators = [Required()])
+    agree=BooleanField('On est gentils mais acceptez les condition d\'utilisation', validators = [Required()])
+    remember_me = BooleanField('Se souvenir', default = False)
